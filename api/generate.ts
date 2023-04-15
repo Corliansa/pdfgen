32a1e2e0939f01affbe545106fe5dba224c1999f
Corliansa Kusumah <corliansa@gmail.com>
2023-04-16 00:02:14 +0200

    use fallback emoji font

--- api/generate.ts
+++ api/generate.ts
@@ -21,12 +21,13 @@ export default async function (req: VercelRequest, res: VercelResponse) {
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>${req.body?.title ?? "Project"}</title>
+      <style>@page{size:letter;margin:1in}body{font-family:system-ui,"Segoe UI",Roboto,Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji"}</style>  
       ${
         req.body?.styled === "true"
           ? `<style>${styled}</style>`
           : `<link rel="stylesheet" href="https://unpkg.com/tailwindcss@3.3.1/src/css/preflight.css" />`
       }
-      <style> @page { size: letter; margin: 1in; } </style>
+      <link href='https://fonts.googleapis.com/css?family=Noto Color Emoji' rel='stylesheet' />
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.css" integrity="sha384-ko6T2DjISesD0S+wOIeHKMyKsHvWpdQ1s/aiaQMbL+TIXx3jg6uyf9hlv3WWfwYv" crossorigin="anonymous" />
       <style>
         ${req.body?.style}
